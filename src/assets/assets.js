39782a79cb9eaa52637e046e46ebf320f237db48
Mohammed Parves <mdperrfan@gmail.com>
2024-11-14 22:28:51 +0600

    updated

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,7 +2,7 @@ import mainprofile from './Profile_main.jpeg'
 import mainprofile2 from './main4.png'
 import aboutimage from './about.png'
 import aboutimage2 from './about2.png'
-import pchat from './projects/pchat.jpg'
+import pchat from './pchat.jpg'
 import contact from './contact2.png'
 export const assets = {
     mainprofile,
