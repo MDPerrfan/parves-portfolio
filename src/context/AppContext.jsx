c088130dc9f09d528136ab930b5d0818ca52fda1
Mohammed Parves <89094860+MDPerrfan@users.noreply.github.com>
2025-08-05 11:38:06 +0600

    Update AppContext.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-    const backendUrl="https://devparvesserver.onrender.com";
+    const backendUrl="https://devparvesserver.vercel.app/";
     const [projectData,setProjectData]=useState([]);
     const[loading,setLoading]=useState()
     const getProjectData=async()=>{
